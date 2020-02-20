a4785454716aeac3b30bc9dd60180902f0891750
superakura <superakura@hotmail.com>
2020-02-20 15:05:51 +0800

    增加layui admin，增加cookie登录

--- ErpMaterial.Web/Controllers/HomeController.cs
+++ ErpMaterial.Web/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ErpMaterial.Web.Models;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace ErpMaterial.Web.Controllers
 {
@@ -37,6 +40,84 @@ namespace ErpMaterial.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Lay()
+        {
+            return View();
+        }
+
+        public IActionResult NoRole()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string userNum, string password, string returnUrl)
+        {
+            try
+            {
+                var userList = new string[] { "admin", "ym" };
+                if (userList.Contains(userNum))
+                {
+                    var user = new ErpMaterial.Models.SysUserInfo();
+                    switch (userNum)
+                    {
+                        case "admin":
+                            user.UserAuthorityList = "admin,truck,notice";
+                            break;
+                        case "ym":
+                            user.UserAuthorityList = "sb";
+                            break;
+                    }
+
+                    #region 登录认证，存入Cookie
+                    //登录认证，存入Cookie
+                    var claims = new List<Claim>(){
+                                  new Claim(ClaimTypes.Name,userNum)
+                               };
+                    foreach (var item in user.UserAuthorityList.Split(","))
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, item));
+                    }
+
+                    //init the identity instances 
+                    var userPrincipal = new ClaimsPrincipal(new ClaimsIdentity(claims, "form"));
+                    //signin 
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal, new AuthenticationProperties
+                    {
+                        ExpiresUtc = DateTime.UtcNow.AddMinutes(20),
+                        IsPersistent = false,
+                        AllowRefresh = false
+                    });
+                    ViewBag.userName = User.Identity.Name;
+                    if (string.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(returnUrl);
+                    #endregion
+                }
+
+                ViewBag.Errormessage = "登录失败，用户名密码不正确";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Errormessage = ex.Message;
+                return View();
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync().Wait();//注销
+            return RedirectToAction("Login", "Home");
         }
     }
 }
--- ErpMaterial.Web/Startup.cs
+++ ErpMaterial.Web/Startup.cs
@@ -12,6 +12,8 @@ using ErpMaterial.Models;
 using ErpMaterial.Service;
 using ErpMaterial.Service.Interface;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 
 namespace ErpMaterial.Web
 {
@@ -27,11 +29,36 @@ namespace ErpMaterial.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services
-                .AddEntityFrameworkSqlServer()
+            //更新数据库步骤
+            //1、删除models下的文件
+            //2、执行命令 Scaffold-DbContext "Data Source=.;Initial Catalog=ErpMaterial;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -O / -F
+            //3、
+
+            services.AddEntityFrameworkSqlServer()
                 .AddDbContext<ErpMaterialContext>(options =>{options.UseSqlServer("Data Source=.;Initial Catalog=ErpMaterial;Integrated Security=True",b => b.UseRowNumberForPaging());});
+
+            #region 依赖注入
             services.AddSingleton(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddTransient<IPlanReportService, PlanReportService>();
+            #endregion
+
+            #region 添加cookie认证服务
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/Home/Login/";
+                options.LogoutPath = new PathString("/login");
+                options.AccessDeniedPath = new PathString("/home/NoRole");
+            });
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            #endregion 
+
             services.AddMvc();
         }
 
@@ -50,6 +77,7 @@ namespace ErpMaterial.Web
 
             app.UseRouting();
             app.UseAuthorization();
+            app.UseCookiePolicy();
 
             app.UseEndpoints(endpoints =>
             {
